09d8636c3c58ed918ec046681d5f10ca02c30563
Ryan Florence <rpflorence@gmail.com>
2023-08-11 12:29:19 -0600

    prep for v2

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  future: {
-    v2_meta: true,
-    v2_routeConvention: true,
-    v2_errorBoundary: true,
-    unstable_dev: true,
-  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
